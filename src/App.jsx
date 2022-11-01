b01deba88fa38b4167b104a893e202416b119589
Şule <sule.kaptan@bil.omu.edu.tr>
2022-11-02 00:35:27 +0300

    added hold-dice & end-game features

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,72 @@
 import React from 'react'
+import Die from "./components/Die"
+import Confetti from "react-confetti"
 import './App.css'
+import {nanoid} from "nanoid"
 
 export default function App() {
+  const [dice, setDice] = React.useState(allNewDice())
+  const [tenzies, setTenzies] = React.useState(false)
+    
+  React.useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld)
+    const firstValue = dice[0].value
+    const allSameValue = dice.every(die => die.value === firstValue)
+    if (allHeld && allSameValue) {
+        setTenzies(true)
+        console.log("You won!")
+    }
+}, [dice])
+
+
+  function generateNewDie() {
+    return {
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid()
+    }
+}
+
+function allNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+        newDice.push(generateNewDie())
+    }
+    return newDice
+}
+
+function rollDice() {
+    setDice(oldDice => oldDice.map(die => {
+        return die.isHeld ? 
+            die :
+            generateNewDie()
+    }))
+}
+
+  function holdDice(id) {
+    setDice(oldDice => oldDice.map(die => {
+      return die.id === id ? 
+          {...die, isHeld: !die.isHeld} :
+          die
+  }))
+}
+
+  const diceElements = dice.map(die => 
+  <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)}/>)
+
   return (
-    <main></main>
+    <main>
+      {tenzies && <Confetti />}
+      <div className='dice-container'>
+        {diceElements}
+      </div>
+      <button 
+        className="roll-dice" 
+        onClick={rollDice}
+        >
+          {tenzies ? "New Game!" : "Roll 'em!"}
+          </button>
+    </main>
 )
 }
 
